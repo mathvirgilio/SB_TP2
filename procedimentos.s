530e3b8facce146556760214b26bc80327c4e55e
mathvirgilio <matheusvirgilio@gmail.com>
2025-02-15 23:46:40 -0300

    FINAL

--- procedimentos.s
+++ procedimentos.s
@@ -4,11 +4,13 @@ section .data
     msg_bloco db 'bloco = %d', 10, 0  ; Mensagem para imprimir os elementos do array
     bloco dd 0, 0, 0, 0
     buffer db '0000000000', 0  ; Buffer para armazenar a string (máx. 10 dígitos + NULL)
+    newline db 10                     ; Nova linha (LF)
 
 
 section .bss
     resto resd 1  ; Reserva um espaço de 4 bytes (1 palavra) para armazenar o valor de eax
     num resb 10   ; Buffer para armazenar a string resultant
+
 
 section .text
     global procedimentos
@@ -33,7 +35,23 @@ loop:
     cmp ecx, 0         ; Se count == 0, sai do loop
     je fim
 
+    mov eax, [edx]
+cont2:
+    push eax
+    push ebx
+    push ecx
+    push edx
+    jmp printar_mem2
+ret2:
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+
     mov eax, [resto]
+    cmp eax, 0
+    je fim
+
     mov ebx, [edx + 4]     ; Pega segundo elemento do bloco
 
     cmp ebx, eax
@@ -43,6 +61,7 @@ loop:
     mov [resto], eax
     add ebx, [edx]
 
+cont:
     push eax
     push ebx
     push ecx
@@ -65,8 +84,11 @@ ret:
 espaco_suficiente:
     mov eax, [edx]
     add eax, [resto]
-    ;mov esi, 0
     add [bloco + esi*4], eax
+    mov ebx, eax
+    mov eax, 0
+    mov [resto], eax
+    jmp cont
 
 printar_mem:
     mov edi, num + 9   ; Aponta para o final do buffer (última posição antes do NULL)
@@ -97,7 +119,54 @@ convert_loop:
     add edx, 10
     sub edx, edi
     int 0x80
+
+    ; Exibir nova linha
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
     jmp ret
+
+printar_mem2:
+    mov edi, num + 9   ; Aponta para o final do buffer (última posição antes do NULL)
+    mov byte [edi], 0  ; Adiciona terminador NULL
+    jmp convert_loop2
+
+convert_loop2:
+    mov edx, 0         ; Zerar EDX para divisão correta
+    mov ecx, 10        ; Divisor = 10
+    div ecx            ; EAX /= 10, EDX = resto (dígito atual)
+    
+    add dl, '0'        ; Converter o número para caractere ASCII
+    dec edi            ; Mover ponteiro para frente
+    mov [edi], dl      ; Armazenar caractere no buffer
+    
+    test eax, eax      ; Verifica se EAX ainda tem dígitos
+    jnz convert_loop2   ; Se sim, continuar loop
+
+    ; Agora, EDI aponta para o início da string convertida.
+    
+    ; Exibir número convertido na saída padrão (opcional)
+    mov eax, 4        ; syscall: sys_write
+    mov ebx, 1        ; saída padrão (stdout)
+    mov ecx, edi      ; ponteiro para string convertida
+    ;mov edx, num + 10 - edi  ; tamanho da string
+    mov edx, 0
+    add edx, num
+    add edx, 10
+    sub edx, edi
+    int 0x80
+
+    ; Exibir nova linha
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
+    jmp ret2
 
 fim:    
     pop ebp
